e03649a2f43c3bf3fdbd632a8cccd2b06b98dee3
mahromir <fellt.mp@gmail.com>
2023-10-31 17:06:14 +0100

    utility

---
+++ utility/dir_gen.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+
+proj_name = sys.argv[1]
+
+
+def get_items_to_gen(sufix: str) -> dict:
+    return {
+        "src": [f"bipol_{sufix}.ipynb"],
+        "output": [],
+        "latex_raport": [f"bipol_{sufix}.tex"],
+    }
+
+    # get current directory
+
+
+path = os.getcwd()
+
+path = os.path.join(path, proj_name)
+print("Created:\t", path)
+
+try:
+    os.mkdir(path)
+except Exception as e:
+    print(e)
+
+to_generate = get_items_to_gen(proj_name)
+for folder in to_generate:
+    new_path = os.path.join(path, folder)
+
+    if not os.path.exists(new_path):
+        os.mkdir(new_path)
+        files_to_gen = to_generate[folder]
+        for file in files_to_gen:
+            new_path_to_file = os.path.join(new_path, file)
+            if not os.path.exists(new_path_to_file):
+                f = open(new_path_to_file, "w")
+                f.close()
+            else:
+                print("Failed to create file! Exiting")
+                exit(-1)
+
+    else:
+        print("Failed to create folder! Exiting")
+        exit(-1)
